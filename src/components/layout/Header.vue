fd1dde5cc795e009be0876814a568e78f2f502ca
Breno Pereira <brenopereira856@gmail.com>
2023-12-02 18:13:18 -0300

    feat: header improvements

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -15,17 +15,36 @@ export default defineComponent({
 
 <template>
     <header class="header">
-        <div class="header__logo">
-            <h1 class="header__logo--image">Brein Store</h1>
-        </div>
-        <div class="header__search">
-            <baseInput 
-                type="text"
-                placeholder="Estou procurando..." 
-                isSearch />
-        </div>
-        <div class="header__user">
-            <button @click="returnTheme">changeTheme</button>
+        <div class="header__content">
+            <div class="header__logo">
+                <h1 class="header__logo--image">Brein Store</h1>
+            </div>
+            <div class="header__search">
+                <baseInput 
+                    type="text"
+                    placeholder="Estou procurando..." 
+                    isSearch />
+            </div>
+            <div class="header__user">
+                <button @click="returnTheme">changeTheme</button>
+            </div>
         </div>
     </header>
 </template>
+
+<style lang="scss">
+.header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: var(--tertiary);
+    transition: all .3s linear;
+
+    &__content {
+        @include container;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+}
+</style>
