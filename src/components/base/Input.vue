66ec20e9a6debdab7ea9caa565e0039c5eff2c8c
Breno Pereira <brenopereira856@gmail.com>
2023-12-02 13:07:18 -0300

    feat: new improvements and components

---
+++ src/components/base/Input.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        type: String,
+        placeholder: String,
+
+        isSearch: Boolean,
+    }
+})
+</script>
+
+<template>
+    <form v-if="isSearch">
+        <input class="search" :type="type" :placeholder="placeholder">
+
+        <button class="search__submit">Pesquisar</button>
+    </form>
+    <input class="search" v-else :type="type" :placeholder="placeholder">
+</template>
+
+<style lang="scss">
+.search {
+    border-radius: 60px;
+    border: 1px solid transparent;
+
+    &__submit {
+        background: transparent;
+        border: 0;
+        font-size: 0;
+    }
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    
+})
+</script>
+
+<template>
+    <header class="header">
+        <div class="header__logo">
+            <h1 class="header__logo--image">Brein Store</h1>
+        </div>
+        <div class="header__search">
+            <baseInput 
+                type="text"
+                placeholder="Estou procurando..." 
+                isSearch />
+        </div>
+        <div class="header__user">
+
+        </div>
+    </header>
+</template>
